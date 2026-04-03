5696ea5124dba9d0ff718b1486da509a470fcb6f
yasirarafaat1 <mailforyasir@gmail.com>
2026-04-03 12:40:07 +0530

    Add custom headers for sitemap.xml to set Content-Type as application/xml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
